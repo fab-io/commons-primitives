bbd1c531f56c624ba987526d9fe546517c8b0f4a
Rodney Waldhoff <rwaldhoff@apache.org>
2003-10-27 18:50:55 +0000

    scrub imports
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/primitives/trunk@141589 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/commons/collections/primitives/adapters/TestByteListList.java
+++ src/test/org/apache/commons/collections/primitives/adapters/TestByteListList.java
@@ -1,5 +1,5 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//primitives/src/test/org/apache/commons/collections/primitives/adapters/TestByteListList.java,v 1.2 2003/10/27 18:50:32 rwaldhoff Exp $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//primitives/src/test/org/apache/commons/collections/primitives/adapters/TestByteListList.java,v 1.3 2003/10/27 18:50:55 rwaldhoff Exp $
  * ====================================================================
  * The Apache Software License, Version 1.1
  *
@@ -58,7 +58,6 @@
 package org.apache.commons.collections.primitives.adapters;
 
 import java.io.Serializable;
-import java.util.Iterator;
 import java.util.List;
 
 import junit.framework.Test;
@@ -69,7 +68,7 @@ import org.apache.commons.collections.primitives.ArrayByteList;
 import org.apache.commons.collections.primitives.RandomAccessByteList;
 
 /**
- * @version $Revision: 1.2 $ $Date: 2003/10/27 18:50:32 $
+ * @version $Revision: 1.3 $ $Date: 2003/10/27 18:50:55 $
  * @author Rodney Waldhoff
  */
 public class TestByteListList extends BaseTestList {
