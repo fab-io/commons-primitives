26bf19e4514a17f2474b2c1af11f864ab1bc494c
Stephen Colebourne <scolebourne@apache.org>
2005-07-24 23:27:06 +0000

    Javadoc fixes bug 35836, from Nathan Beyer
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/primitives/trunk@224673 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/collections/primitives/IntIterator.java
+++ src/java/org/apache/commons/collections/primitives/IntIterator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2004 The Apache Software Foundation
+ * Copyright 2003-2005 The Apache Software Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ public interface IntIterator {
      * Returns the next element in me.
      * 
      * @return the next element in me
-     * @throws NoSuchElementException if there is no next element
+     * @throws java.util.NoSuchElementException if there is no next element
      */          
     int next();
     
